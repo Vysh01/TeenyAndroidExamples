8db085f05224b87e8cb5c4f332dd9e6cff16b4f5
Vishal Shrestha <vishal.shrestha9794@gmail.com>
2023-05-13 01:25:59 +0545

    download using download manager added

--- app/src/main/java/com/thecodecity/teenyexamples/ActivityDownload.java
+++ app/src/main/java/com/thecodecity/teenyexamples/ActivityDownload.java
@@ -1,9 +1,14 @@
 package com.thecodecity.teenyexamples;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,8 +28,32 @@ public class ActivityDownload extends Activity {
         btnDownload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(editText.getText().toString())));
+                AlertDialog.Builder builder = new AlertDialog.Builder(ActivityDownload.this);
+                builder.setMessage("Choose Download Type");
+                builder.setPositiveButton("Using Default Browser", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(editText.getText().toString())));
+                    }
+                });
+                builder.setNegativeButton("Using Download Manager", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        downloadPdf(editText.getText().toString());
+                    }
+                });
             }
         });
+
+    }
+
+    private void downloadPdf(String url) {
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setTitle("PDF Download");
+        request.setDescription("Downloading PDF file");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "pdf_file.pdf");
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
     }
 }
